de1a9188ba1f1464a180aff96f53e88bc5426c0e
萧 <1243925457@qq.com>
2022-10-11 08:23:54 +0800

    读取软盘数据到内存

--- myOS.asm
+++ myOS.asm
@@ -23,32 +23,84 @@ DB "CHERRY-OS  " ;磁盘的名称（11个字节）
 DB "FAT12   " ;磁盘的格式名称（8字节）
 TIMES 18 DB 0; 先空出18字节 这里与原文写法不同
 
+FirstSectorOfRootDirectory	equ	19
+SectorNumberOfTrack	equ	18
+BaseOfLoader	equ	0x9000
+OffSetOfLoader	equ	0x0820
+BaseOfFATEntry	equ	0x1000
+
 ;程序核心
 entry:
-    MOV AX,0  ;初始化寄存器
-    MOV SS,AX
-    MOV SP,0x7c00
-    MOV DS,AX
-    MOV ES,AX
-    MOV SI,msg
-putloop:
-    MOV AL,[SI]
-    ADD SI,1
-    CMP AL,0
-    JE fin
-    MOV AH,0x0e ;显示一个文字
-    MOV BX,15 ;指定字符的颜色
-    INT 0x10 ;调用显卡BIOS
-    JMP putloop
+    	MOV AX,0  ;初始化寄存器
+    	MOV SS,AX
+    	MOV SP,0x7c00
+    	MOV DS,AX
+    	MOV ES,AX
+	; JMP putloop
+read_file_ready: ;读软盘准备
+	MOV DI, OffSetOfLoader
+	MOV AH, 02 ;指明读扇区功能调用
+	MOV AL, 1 ;指明要读的扇区数为1
+	MOV DL, 0x00 ;指明要读的驱动为A
+	MOV DH, 0 ;指定读取的磁头0
+	MOV CH, 0 ;柱面0
+	MOV CL, 2 ;读取扇区2
+	;下面三句指定读取到内存地址0x0820处
+	MOV AX, 0x0820
+	MOV ES, AX
+	MOV BX, 0 ;数据读取后放到的内存地址
+read_file:
+	INT 13H ;调用BIOS文件读取
+	JNC read_file_loop
+	MOV SI, read_file_error_msg
+	JMP putloop
+read_file_loop: ;循环读取内容，循环的顺序是扇区->磁头->柱面，不知道这里面是否有什么说法？可以调换吗？
+	;把内存地址后移0x200
+	MOV AX, ES
+	ADD AX, 0x0020
+	MOV ES, AX
+	ADD CL, 1 ;加1，读取下一个扇区
+	CMP CL, 18 ;如果CL扇区大于软盘总扇区数18，则说明读取完成，不再读取
+	JBE read_file
+	; 上面是扇区的循环，完毕后到磁头的循环,重置扇区，增加磁头到反面1
+	MOV CL, 1
+	ADD DH, 1
+	CMP DH, 2
+	JB read_file
+	;上面都完成后，到柱面的读取循环，重置扇区(前面已重置）和磁头，增加柱面（不知道为啥书中不是80而是10）
+	MOV DH, 0
+	ADD CH, 1
+	CMP CH, 10
+	JB read_file
+loader_end: ;启动程序加载完成
+	MOV AL,0x0a
+    	MOV AH,0x0e ;显示一个文字
+    	MOV BX,15 ;指定字符的颜色
+    	INT 0x10 ;调用显卡BIOS
+	MOV SI,msg
+putloop: ;加载完成，成功显示
+    	MOV AL,[SI]
+    	ADD SI,1
+    	CMP AL,0
+    	JE fin
+    	MOV AH,0x0e ;显示一个文字
+    	MOV BX,15 ;指定字符的颜色
+    	INT 0x10 ;调用显卡BIOS
+    	JMP putloop
 fin:
-    HLT ;CPU停止,等待指令
-    JMP fin ;无限循环
+    	HLT ;CPU停止,等待指令
+    	JMP fin ;无限循环
 msg:
-    DB 0x0a , 0x0a ;换行两次
-    DB "hello, my OS"
-    DB 0x0a
-    DB 0
+    	DB 0x0a , 0x0a ;换行两次
+    	DB "hello, my OS, boot loader end"
+    	DB 0x0a
+    	DB 0
+read_file_error_msg:
+    	DB 0x0a , 0x0a ;换行两次
+    	DB "read file error!!!"
+    	DB 0x0a
+    	DB 0
     
-    TIMES 0x1fe-($-$$) DB 0 ;填写0x00,直到0x001fe
-    
-    DB 0x55, 0xaa
+boot_flag: ;启动区标识
+    	TIMES 0x1fe-($-$$) DB 0 ;填写0x00,直到0x001fe
+    	DB 0x55, 0xaa

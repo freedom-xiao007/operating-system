bc56c90d69bd2eeb12d509dc5dfcc0f2aaa44480
萧 <1243925457@qq.com>
2022-10-12 09:13:51 +0800

    软盘数据读取并显示完成

--- myOS.asm
+++ myOS.asm
@@ -23,11 +23,7 @@ DB "CHERRY-OS  " ;磁盘的名称（11个字节）
 DB "FAT12   " ;磁盘的格式名称（8字节）
 TIMES 18 DB 0; 先空出18字节 这里与原文写法不同
 
-FirstSectorOfRootDirectory	equ	19
-SectorNumberOfTrack	equ	18
-BaseOfLoader	equ	0x9000
 OffSetOfLoader	equ	0x0820
-BaseOfFATEntry	equ	0x1000
 
 ;程序核心
 entry:
@@ -36,24 +32,31 @@ entry:
     	MOV SP,0x7c00
     	MOV DS,AX
     	MOV ES,AX
-	; JMP putloop
 read_file_ready: ;读软盘准备
-	MOV DI, OffSetOfLoader
 	MOV AH, 02 ;指明读扇区功能调用
 	MOV AL, 1 ;指明要读的扇区数为1
 	MOV DL, 0x00 ;指明要读的驱动为A
 	MOV DH, 0 ;指定读取的磁头0
 	MOV CH, 0 ;柱面0
-	MOV CL, 2 ;读取扇区2
+	MOV CL, 1 ;读取扇区1（因为目前没有其他文件，就只有扇区1的启动区数据，所以我们这从1开始，不是从2开始）
 	;下面三句指定读取到内存地址0x0820处
-	MOV AX, 0x0820
+	MOV AX, OffSetOfLoader
 	MOV ES, AX
 	MOV BX, 0 ;数据读取后放到的内存地址
 read_file:
+	;调试用，用于查看读取磁盘是否达到了循环次数,call相当于函数调用
+	MOV SI, read_file_msg
+	CALL func_show_msg
+
+	;前面调用函数时，改变了有关的寄存器，这里重置回来（也可以使用栈，但这个方便点）
+	MOV AH, 02 ;指明读扇区功能调用
+	MOV AL, 1 ;指明要读的扇区数为1
+	MOV BX, 0 ;数据读取后放到的内存地址
+
 	INT 13H ;调用BIOS文件读取
 	JNC read_file_loop
 	MOV SI, read_file_error_msg
-	JMP putloop
+	JMP show_msg_info
 read_file_loop: ;循环读取内容，循环的顺序是扇区->磁头->柱面，不知道这里面是否有什么说法？可以调换吗？
 	;把内存地址后移0x200
 	MOV AX, ES
@@ -72,13 +75,26 @@ read_file_loop: ;循环读取内容，循环的顺序是扇区->磁头->柱面�
 	ADD CH, 1
 	CMP CH, 10
 	JB read_file
+show_mem_file: ;打印显示刚才加载的文件内容
+	MOV AX, OffSetOfLoader
+	MOV ES, AX
+	MOV DI, 0
+show_mem_byte: ;整个数据也就512，所以循环512次即可
+	MOV AL, BYTE [ES:DI]
+	CMP DI, 512
+	JE loader_end
+	ADD DI, 1
+	MOV AH,0x0e ;显示一个文字
+    	MOV BX,15 ;指定字符的颜色
+    	INT 0x10 ;调用显卡BIOS	
+	JMP show_mem_byte
 loader_end: ;启动程序加载完成
 	MOV AL,0x0a
     	MOV AH,0x0e ;显示一个文字
     	MOV BX,15 ;指定字符的颜色
     	INT 0x10 ;调用显卡BIOS
 	MOV SI,msg
-putloop: ;加载完成，成功显示
+show_msg_info: ;加载完成，成功显示
     	MOV AL,[SI]
     	ADD SI,1
     	CMP AL,0
@@ -86,18 +102,37 @@ putloop: ;加载完成，成功显示
     	MOV AH,0x0e ;显示一个文字
     	MOV BX,15 ;指定字符的颜色
     	INT 0x10 ;调用显卡BIOS
-    	JMP putloop
+    	JMP show_msg_info
 fin:
     	HLT ;CPU停止,等待指令
     	JMP fin ;无限循环
-msg:
-    	DB 0x0a , 0x0a ;换行两次
-    	DB "hello, my OS, boot loader end"
-    	DB 0x0a
+func_show_msg:
+    	MOV AL,[SI]
+    	ADD SI,1
+    	CMP AL,0
+    	JE func_ret
+    	MOV AH,0x0e ;显示一个文字
+    	MOV BX,15 ;指定字符的颜色
+    	INT 0x10 ;调用显卡BIOS
+    	JMP func_show_msg
+func_ret:
+	RET
+read_file_msg:
+    	DB "r"
     	DB 0
 read_file_error_msg:
     	DB 0x0a , 0x0a ;换行两次
     	DB "read file error!!!"
+    	DB 0x0a
+    	DB 0
+show_mem_info_msg:
+	DB 0x0a , 0x0a ;换行两次
+    	DB "start show mem file!!!"
+    	DB 0x0a
+    	DB 0
+msg:
+    	DB 0x0a , 0x0a ;换行两次
+    	DB "hello, my OS, boot loader end"
     	DB 0x0a
     	DB 0
     

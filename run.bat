6c5670d35fb7ca1d539d3a2df64f86c836eb8902
萧 <1243925457@qq.com>
2022-11-14 11:06:34 +0800

    Update run.bat

--- run.bat
+++ run.bat
@@ -2,7 +2,7 @@ D:\\software\\NASM\\nasm.exe bootsect.asm -o bootsect.bin -l bootsect.lst
 D:\\software\\NASM\\nasm.exe setup.asm -o setup.bin  -l setup.lst
 
 gcc -m32 -fno-asynchronous-unwind-tables -s -O2 -c -o .\\c\\start.o .\\c\\start.c
-D:\\software\\objconv\\objconv.exe -fnasm .\\c\\start.o .\\c\\start.asm
+D:\\software\\objconv\\objconv.exe -fnasm .\\c\\start.o .\\c\\nasm\\start.asm
 D:\\software\\python3\\python.exe E:\\code\\python\\self\\tools\\tools\\objconv2nasm_clearn.py
 copy /B head.asm+.\\c\\clean\\start.asm+func.asm  kernel.asm
 D:\\software\\NASM\\nasm.exe kernel.asm -o kernel.bin -l kernel.lst

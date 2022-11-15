85d5f431c28f0f9f089c1b5ec65b6ce84d5180e0
萧 <1243925457@qq.com>
2022-11-15 09:00:40 +0800

    色号设定

--- c/start.c
+++ c/start.c
@@ -1,15 +1,70 @@
-/* 告诉C编译器，有一个函数在别的文件里 */
-typedef unsigned char uint8_t;
+void io_hlt(void);
+void io_cli(void);
+void io_out8(int port, int data);
+int io_load_eflags(void);
+void io_store_eflags(int eflags);
 
-void put_str(uint8_t* message);
+/*就算写在同一个源文件里，如果想在定义前使用，还是必须事先声明一下。*/
 
-void start(void) {
-    put_str("0123456789");
-    int i;
-    char *p;
-    p = (char *) 0xa0000; /*将地址赋值进去*/
+void init_palette(void);
+void set_palette(int start, int end, unsigned char *rgb);
+
+void start(void)
+{
+    int i; /* 声明变量。变量i是32位整数型 */
+    char *p; /* 变量p是BYTE [...]用的地址 */
+
+    init_palette(); /* 设定调色板 */
+
+    p = (char *) 0xa0000; /* 指定地址 */
+
     for (i = 0; i <= 0xffff; i++) {
-    	p[i] = i & 0x0f;
+        p[i] = i & 0x0f;
     }
-    while(1);
+
+    for (; ; ) {
+        io_hlt();
+    }
+}
+
+void init_palette(void)
+{
+    static unsigned char table_rgb[16 * 3] = {
+        0x00, 0x00, 0x00,   /*  0：黑 */
+        0xff, 0x00, 0x00, /* 1：亮红 */
+        0x00, 0xff, 0x00, /* 2：亮绿 */
+        0xff, 0xff, 0x00, /* 3：亮黄 */
+        0x00, 0x00, 0xff, /* 4：亮蓝 */
+        0xff, 0x00, 0xff, /* 5：亮紫 */
+        0x00, 0xff, 0xff, /* 6：浅亮蓝 */
+        0xff, 0xff, 0xff, /* 7：白 */
+        0xc6, 0xc6, 0xc6, /* 8：亮灰 */
+        0x84, 0x00, 0x00, /* 9：暗红 */
+        0x00, 0x84, 0x00, /* 10：暗绿 */
+        0x84, 0x84, 0x00, /* 11：暗黄 */
+        0x00, 0x00, 0x84, /* 12：暗青 */
+        0x84, 0x00, 0x84, /* 13：暗紫 */
+        0x00, 0x84, 0x84, /* 14：浅暗蓝 */
+        0x84, 0x84, 0x84 /* 15：暗灰 */
+    };
+    set_palette(0, 15, table_rgb);
+    return;
+
+    /* C语言中的static char语句只能用于数据，相当于汇编中的DB指令 */
+}
+
+void set_palette(int start, int end, unsigned char *rgb)
+{
+    int i, eflags;
+    eflags = io_load_eflags();  /* 记录中断许可标志的值*/
+    io_cli();                      /* 将中断许可标志置为0，禁止中断 */
+    io_out8(0x03c8, start);
+    for (i = start; i <= end; i++) {
+        io_out8(0x03c9, rgb[0] / 4);
+        io_out8(0x03c9, rgb[1] / 4);
+        io_out8(0x03c9, rgb[2] / 4);
+        rgb += 3;
+    }
+    io_store_eflags(eflags);     /* 复原中断许可标志 */
+    return;
 }
--- func.asm
+++ func.asm
@@ -1,6 +1,58 @@
-_put_str:
-	mov  esi, esp ;保护模式DS=0,数据用绝对地址访问
-	mov  cl, 0x09                   ;蓝色
-	mov  edi, 0xb8000+22*160        ;指定显示在某行,显卡内存地址需用绝对地址
-	call printnew                   ;0xb8000为字符模式下显卡映射到的内存地址 
-	ret  
+[BITS 32]                          ; 制作32位模式用的机器语言
+        GLOBAL  _io_hlt, _io_cli, _io_sti, io_stihlt
+        GLOBAL  _io_in8,  _io_in16,  _io_in32
+        GLOBAL  _io_out8, _io_out16, _io_out32
+        GLOBAL  _io_load_eflags, _io_store_eflags
+[SECTION .text]
+_io_hlt:     ; void io_hlt(void);
+        HLT
+        RET
+_io_cli:     ; void io_cli(void);
+        CLI
+        RET
+_io_sti:     ; void io_sti(void);
+        STI
+        RET
+_io_stihlt: ; void io_stihlt(void);
+        STI
+        HLT
+        RET
+_io_in8:     ; int io_in8(int port);
+        MOV      EDX, [ESP+4]      ; port
+        MOV      EAX,0
+        IN       AL, DX
+        RET
+_io_in16:   ; int io_in16(int port);
+
+        MOV      EDX, [ESP+4]      ; port
+        MOV      EAX,0
+        IN       AX, DX
+        RET
+_io_in32:   ; int io_in32(int port);
+        MOV      EDX, [ESP+4]      ; port
+        IN       EAX, DX
+        RET
+_io_out8:   ; void io_out8(int port, int data);
+        MOV      EDX, [ESP+4]      ; port
+        MOV      AL, [ESP+8]       ; data
+        OUT      DX, AL
+        RET
+_io_out16:  ; void io_out16(int port, int data);
+        MOV      EDX, [ESP+4]      ; port
+        MOV      EAX, [ESP+8]      ; data
+        OUT      DX, AX
+        RET
+_io_out32:  ; void io_out32(int port, int data);
+        MOV      EDX, [ESP+4]      ; port
+        MOV      EAX, [ESP+8]      ; data
+        OUT      DX, EAX
+        RET
+_io_load_eflags:     ; int io_load_eflags(void);
+        PUSHFD       ; 指PUSH EFLAGS
+        POP      EAX
+        RET
+_io_store_eflags:   ; void io_store_eflags(int eflags);
+        MOV      EAX, [ESP+4]
+        PUSH EAX
+        POPFD ; 指POP EFLAGS
+        RET

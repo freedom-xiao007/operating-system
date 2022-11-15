f38a91e02fffc3790d1cf95e41c145396dfd9408
萧 <1243925457@qq.com>
2022-11-15 08:33:55 +0800

    原始指针

--- func.asm
+++ func.asm
@@ -4,9 +4,3 @@ _put_str:
 	mov  edi, 0xb8000+22*160        ;指定显示在某行,显卡内存地址需用绝对地址
 	call printnew                   ;0xb8000为字符模式下显卡映射到的内存地址 
 	ret  
-
-_write_mem8:     ; void write_mem8(int addr, int data);
-        MOV      ECX, [ESP+4]      ; [ESP + 4]中存放的是地址，将其读入ECX
-        MOV      AL, [ESP+8]       ; [ESP + 8]中存放的是数据，将其读入AL
-        MOV      [ECX], AL
-        RET
